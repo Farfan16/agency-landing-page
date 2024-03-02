7f2a1a2f2432860407980b3a9745569028fbb0b8
Farrel Fanny <farrelexamples@gmail.com>
2024-03-02 13:51:39 +0700

    add header, fifth, and footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import Fifth from "./sections/Fifth";
 import First from "./sections/First";
+import Footer from "./sections/Footer";
 import Fourth from "./sections/Fourth";
+import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Second from "./sections/Second";
 import Third from "./sections/Third";
@@ -7,11 +10,14 @@ import Third from "./sections/Third";
 function App() {
   return (
     <main className="min-h-screen w-full flex flex-col font-Barlow">
+      <Header />
       <Hero />
       <First />
       <Second />
       <Third />
       <Fourth />
+      <Fifth />
+      <Footer />
     </main>
   );
 }
